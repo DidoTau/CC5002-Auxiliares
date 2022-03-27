1bd57c6c7cf3379918867c3092e45161d22e096d
Esteban Ramirez <43161063+DidoTau@users.noreply.github.com>
2022-03-27 19:00:29 -0300

    agrego aux 3

---
+++ Auxiliar3/cgi-bin/formulario.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import cgi 
+import cgitb 
+
+cgitb.enable()
+print("Content-type:text/html")
+print()
+form = cgi.FieldStorage()
+
+print("<html><body>Buena hermanito, los datos que ingresaste son: <br>")
+for key in form.keys():
+    print("""
+        <p> %s %s </p>
+            """%(key, form[key].value))
+
+print("</body></html>")
